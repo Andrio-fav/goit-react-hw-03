70d2cdefda778a5a768486db56e18345caca32da
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-05-18 22:15:44 +0200

    add all file

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,20 @@
+import css from './Contact.module.css';
+
+export default function Contact({ id, name, number, Ondelete }) {
+    return (
+        <li className={css.wrapper}>
+            <div>
+                <p className={css.text}>
+                    {name}
+                </p>
+                <p className={css.text}>
+                    {number}
+                </p>
+            </div>
+
+            <button className={css.deleteButton} id={id} onClick={Ondelete}>
+                Delete
+            </button>
+        </li>
+    )
+}
